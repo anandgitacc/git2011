212dfa9ee56ee6277b4a474c88443734ef9c93e8
anandgitacc <anand.akmail@gmail.com>
2021-11-22 15:17:48 +0530

    testgit

---
+++ src/org/gitproject/SampleOne.java
@@ -0,0 +1,24 @@
+package org.gitproject;
+
+public class SampleOne {
+	
+	public void empName() {
+		
+		System.out.println("anand");
+	}
+	
+	public void empMobno() {
+		
+		System.out.println("8939278836");
+	}
+	
+		
+public static void main(String[] args) {
+		
+	SampleOne s = new SampleOne();
+	
+	s.empName();
+	s.empMobno();
+}
+
+}
